89d7c407fc1edf3a1b93ee278d4531a98a4db78e
Admin <d>
2020-03-03 22:05:58 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,46 +1,79 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+  let keys = Object.keys(MORSE_TABLE),
+    morseNumbers = {};
+  keys.forEach(key => {
+    let arr = key.split('');
+    let resultArr = arr.map(elem => {
+      if (elem === '.') return '10';
+      else if (elem === '-') return '11'
+    });
+    for (let i = resultArr.length * 2; i < 10; i++) {
+      resultArr.unshift('0')
+    }
+    morseNumbers[resultArr.join('')] = MORSE_TABLE[key]
+  });
+  morseNumbers['**********'] = ' ';
+
+  let exprArr = expr.split(''),
+    tempArr = [],
+    letterCodesArr = [];
+
+
+  exprArr.forEach((value) => {
+      tempArr.push(value);
+    if (tempArr.length === 10) {
+      letterCodesArr.push(tempArr.join(''));
+      tempArr = [];
+    }
+  });
+
+
+  return letterCodesArr.map(elem => {
+      return morseNumbers[elem]
+  }).join('');
 }
+
+
 
 module.exports = {
     decode
-}
+};
